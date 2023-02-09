47fc6c1b3265a3fe32713d3f897dcf7b739c2877
Parker Nix <pcn23@drexel.edu>
2023-02-09 12:00:29 -0500

    Button, text field, etc. functionaility init

--- idm262-pcn23/ViewController.swift
+++ idm262-pcn23/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    var daCounter:Int = 0
+    
+    @IBOutlet weak var usernameText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.textColor = UIColor(named: "Primary")
     }
-
-
+    
+    @IBAction func thingDoer(_ sender: Any) {
+        daCounter += 1
+        displayLabel.text = String(daCounter)
+    }
+    
+    @IBAction func thingPrinter(_ sender: Any) {
+        displayLabel.text = "Welcome, \(usernameText.text!)!"
+    }
+    
+    // Disable keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
